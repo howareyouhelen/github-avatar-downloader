bb87ec67c51a63e2fec7e5caa3884aa4803d46d3
Helen <howareyouhelen@hotmail.com>
2017-07-04 00:22:32 +0000

    make HTTP request to request func

--- download_avatars.js
+++ download_avatars.js
@@ -2,14 +2,29 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+
 var GITHUB_USER = "YOUR USERNAME HERE";
 var GITHUB_TOKEN = "YOUR ACCESSTOKEN HERE";
 
+
+function options(path){
+  return {
+    url: "https://api.github.com" + path,
+    headers: {
+      "user-agent": "yogurt"
+    },
+    qs: {
+      access_token: process.env.GITHUB_ACCESS_TOKEN
+    }
+  };
+}
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
-
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-
-console.log(requestURL)
+  const path = `/repos/${repoOwner}/${repoName}/contributors`;
+  request(options(path), function (err, response, body) {
+    console.log(body)
+})
 }
 
   getRepoContributors("jquery", "jquery", function(err, result) {
